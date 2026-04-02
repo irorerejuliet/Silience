7d94724011793bc6ccf41bd2777cb8f0e9ed1a8a
irorere juliet <irorerejuliet@gmail.com>
2026-04-02 22:46:10 +0100

    Created Add to cartpage build still having  errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layouts/Navbar";
+import CartDrawer from "@/components/CartDrawer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Navbar />
         {children}
+        <CartDrawer/>
       </body>
     </html>
   );
