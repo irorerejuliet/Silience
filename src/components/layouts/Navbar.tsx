42cf428f2ecc1f4b61f72802a4d0309713eded6e
irorere juliet <irorerejuliet@gmail.com>
2026-04-03 01:06:24 +0100

    added fixed style to the nav bar

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,29 +1,48 @@
-
-import Link from 'next/link';
-
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <header>
-      <nav
-        className="w-full bg-white px-8 py-4 lg:flex hidden items-center justify-between text-black
-      "
-      >
-        <Link href="/" className="text-3xl font-bold tracking-wide">SILENCE PLEASE</Link>
+      <nav className="fixed top-0 left-0 w-full bg-white px-8 py-4 lg:flex hidden items-center justify-between text-black z-50">
+        <Link href="/" className="text-3xl font-bold tracking-wide">
+          SILENCE PLEASE
+        </Link>
 
-        <ul className="flex gap-6 text-sm font-medium cursor-pointer hover:underline">
-          <Link href="/speakers">Speakers</Link>
-          <Link href="/event">Events</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-          <Link href="/sigin">Log in</Link>
-          <Link href="/cart" >
-            Cart (0)
-          </Link>
+        <ul className="flex gap-6 text-sm font-medium">
+          <li>
+            <Link href="/speakers" className="hover:underline">
+              Speakers
+            </Link>
+          </li>
+          <li>
+            <Link href="/event" className="hover:underline">
+              Events
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="hover:underline">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="hover:underline">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link href="/sigin" className="hover:underline">
+              Log in
+            </Link>
+          </li>
+          <li>
+            <Link href="/cart" className="hover:underline">
+              Cart (0)
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
